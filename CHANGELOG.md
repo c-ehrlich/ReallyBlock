51516d62b512b8e3dd82ac30a88ab5c42e8c3462
c-ehrlich <ehrlich.christopher@gmail.com>
2023-09-02 10:05:16 +0200

    only check added nodes and their children

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## 0.0.4
+- improve performance by only searching the added nodes and their child nodes instead of the entire document
+
+## 0.0.3
+- significantly improve performance by only checking for nodes to remove if nodes were added
+
 ## 0.0.2
 - Add changelog
 - Remove initial run pre MutationObserver, which doesn't find elements because the main Discord app hasn't mounted yet
--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,15 @@
-function findAndDeleteBlockedMessages() {
-  const messages = document.querySelectorAll("[class^=blockedSystemMessage-]");
-  const containers = [...messages].map(item => item.parentElement.parentElement.parentElement);
-  containers.forEach(item => item.style.display = "none");
+function findAndDeleteBlockedMessages(addedNodes) {
+  addedNodes.forEach(node => {
+    const messages = node.querySelectorAll("[class^=blockedSystemMessage-]");
+    const containers = [...messages].map(item => item.parentElement.parentElement.parentElement);
+    containers.forEach((item) => item.style.display = "none");
 
-  const questions = document.querySelectorAll("[class^=firstMessageContent-")
-  const questionContainers = [...questions]
-    .filter(item => item.textContent.includes("Post contains a blocked message"))
-    .map(item => item.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement);
-  questionContainers.forEach(item => item.style.display = "none");
+    const questions = node.querySelectorAll("[class^=firstMessageContent-")
+    const questionContainers = [...questions]
+      .filter(item => item.textContent.includes("Post contains a blocked message"))
+      .map(item => item.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement);
+    questionContainers.forEach((item) => item.style.display = "none");
+  });
 }
 
 const targetNode = document.querySelector("body");
@@ -15,8 +17,7 @@ const config = { attributes: true, childList: true, subtree: true };
 const observer = new MutationObserver((mutationList, _observer) => {
   for (const mutation of mutationList) {
     if (mutation.addedNodes.length > 0) {
-      // TODO: only check the added nodes
-      findAndDeleteBlockedMessages();
+      findAndDeleteBlockedMessages(mutation.addedNodes);
     }
   }
 });
